0c0406e8195635acc282fd058611ed574658c20d
HU Pili <hpl1989@gmail.com>
2015-08-24 23:41:29 +0800

    add user scripts

---
+++ Imageholics.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         Imageholics
+// @namespace    http://initiumlab.com/
+// @version      0.1
+// @description  enter something useful
+// @author       You
+// @match        https://theinitium.com/*
+// @grant        none
+// ==/UserScript==
+
+$(function(){
+    var images = $('figure img')
+    images.map(function(){
+        $(this).css('max-height', '200px').css('max-width', '100%')
+    })
+    console.log(images)
+    var article = $('article')
+    var list = $('<ul>')
+    list.css('max-width', '100%')
+    for (var i=0; i < images.length; i++){
+        var li = $('<li>').append(images[i])
+        li.css('display', 'inline')
+        list.append(li)
+    }
+    article.before(list)
+})
