8d991bbfce8406ddddfc647164d5515f5a613bb8
HU Pili <hpl1989@gmail.com>
2015-09-03 13:48:40 +0800

    use freewall to layout as bricks

--- Imageholics.user.js
+++ Imageholics.user.js
@@ -6,10 +6,10 @@
 // @author       You
 // @match        https://theinitium.com/*
 // @grant        none
+// @require      https://raw.githubusercontent.com/kombai/freewall/master/freewall.min.js
 // ==/UserScript==
 
 $(function(){
-    var awesomeBar = $('<div>')
     var images = $('figure.image')
     var images = images.map(function(){
         var oldFigure = $(this)
@@ -24,17 +24,45 @@ $(function(){
                 scrollTop: oldFigure.offset().top
             }, 800);
         })
+        //newFigure.addClass('brick')
+        newFigure.find('figcaption').hide()
         return newFigure
     })
     console.log(images)
     var article = $('article')
-    var list = $('<ul>')
+    var list = $('<div id="freewall">')
     list.css('max-width', '100%')
     for (var i=0; i < images.length; i++){
-        var li = $('<li>').append(images[i])
+        var li = $('<div class="brick">').append(images[i])
         li.css('display', 'inline')
         list.append(li)
     }
+    var awesomeBar = $('<div id="awesome-bar">')
     awesomeBar.append(list)
+    awesomeBar
+    //.css('width', '800px')
+    //.css('height', '1000px')
+    .css('margin-bottom', '3em')
+    .css('margin-top', '3em')
     awesomeBar.insertAfter(article.find('h1'))
+    var wall = new freewall('#freewall')  
+    wall.reset({
+        selector: '.brick',
+        animate: true,
+        cellW: 200,
+        cellH: 'auto',
+        onResize: function() {
+            console.log('on resize')
+            wall.fitWidth()
+        }
+    });
+    //awesomeBar.width('700px')
+    window.setTimeout(function(){
+        console.log('delayed fit width')
+        wall.fitWidth()
+    }, 200)
+    window.setTimeout(function(){
+        console.log('delayed fit width')
+        wall.fitWidth()
+    }, 500)
 })
