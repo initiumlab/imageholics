c7ba6ddcb36948d4f6ead98b660141b5e43fe3a6
HU Pili <hpl1989@gmail.com>
2015-09-03 16:37:07 +0800

    better layout; refactor code

--- Imageholics.user.js
+++ Imageholics.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Imageholics
 // @namespace    http://initiumlab.com/
-// @version      0.2
+// @version      0.3
 // @description  Awesome experience for image-aholics
 // @author       Pili Hu @ Initium Lab
 // @match        https://theinitium.com/*
 // @grant        none
-// @require      https://raw.githubusercontent.com/kombai/freewall/master/freewall.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/freewall/1.0.5/freewall.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/sugar/1.4.1/sugar-full.min.js
 // ==/UserScript==
 
@@ -23,6 +23,36 @@ var addAltToImage = function(jqFigure){
     image.attr('alt', text)
 }
 
+var processFigure = function(jqFigure){
+    var oldFigure = jqFigure
+    var newFigure = oldFigure.clone()
+    addAltToImage(newFigure)
+    addAltToImage(oldFigure)
+    
+    newFigure.find('img').css('max-width', '100%')
+    
+    newFigure
+    .css('max-width', '200px')
+    .css('margin', '0.5em')
+    .css('display', 'inline-block')
+    //.css('max-height', '200px')
+    
+    newFigure.click(function(){
+        $('html, body').animate({
+            scrollTop: oldFigure.offset().top
+        }, 800)
+    })
+
+    newFigure.css('cursor', 'pointer')
+    newFigure.hover(function() {
+        newFigure.addClass('enlarge')
+    }, function() {
+        newFigure.removeClass('enlarge')
+    })
+    
+    return newFigure
+}
+
 $(function(){
     var stylesheet = document.styleSheets[0]
     var globalStyles = [
@@ -32,39 +62,19 @@ $(function(){
         stylesheet.insertRule(globalStyles[i])
     }
     
-    var images = $('figure.image')
-    var images = images.map(function(){
+    var figures = $('figure.image')
+    var figures = figures.map(function(){
         var oldFigure = $(this)
-        var newFigure = oldFigure.clone()
-        newFigure.find('img').css('max-width', '100%')
-        newFigure
-        .css('max-width', '200px')
-        .css('display', 'inline-block')
-            //.css('max-height', '200px')
-        newFigure.click(function(){
-            $('html, body').animate({
-                scrollTop: oldFigure.offset().top
-            }, 800);
-        })
-
-        addAltToImage(newFigure)
-        addAltToImage(oldFigure)
-        
-        newFigure.css('cursor', 'pointer')
-        newFigure.hover(function() {
-            newFigure.addClass('enlarge');
-        }, function() {
-            newFigure.removeClass('enlarge');
-        });
+        var newFigure = processFigure(oldFigure)
         return newFigure
     })
     
-    console.log(images)
+    console.log(figures)
     var article = $('article')
     var list = $('<div id="freewall">')
     list.css('max-width', '100%')
-    for (var i=0; i < images.length; i++){
-        var li = $('<div class="brick">').append(images[i])
+    for (var i=0; i < figures.length; i++){
+        var li = $('<div class="brick">').append(figures[i])
         li.css('display', 'inline')
         list.append(li)
     }
